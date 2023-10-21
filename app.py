59853be75d4e64e08738420167d37f41b5d86c9f
gmacheta <82578499+gmacheta@users.noreply.github.com>
2023-10-21 18:48:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template, request, redirect, send_from_directory, session
+from flask import Flask, render_template, request, redirect, send_from_directory, session, flash
 import zipfile
 import os
 from werkzeug.utils import secure_filename
-from multiprocessing import Process
-
-
-
-
-
+from tempfile import TemporaryDirectory
+import matplotlib.pyplot as plt
+from cell_count import generate_plot  # Import your generate_plot function here
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Add a secret key for session
 
 @app.route('/')
 def home():
@@ -17,52 +15,62 @@ def home():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    if 'zip_file' not in request.files:
+    if 'zip_file1' not in request.files or 'zip_file2' not in request.files:
+        flash('Please select two ZIP files.')
         return redirect(request.url)
 
-    file = request.files['zip_file']
+    zip_file1 = request.files['zip_file1']
+    zip_file2 = request.files['zip_file2']
 
-    if file.filename == '':
+    if zip_file1.filename == '' or not zip_file1.filename.endswith('.zip'):
+        flash(f'Invalid ZIP file: {zip_file1.filename}')
         return redirect(request.url)
 
-    if file and file.filename.endswith('.zip'):
-        zip_filename = secure_filename(file.filename)
-        session['uploaded_zip_filename'] = zip_filename
+    if zip_file2.filename == '' or not zip_file2.filename.endswith('.zip'):
+        flash(f'Invalid ZIP file: {zip_file2.filename}')
+        return redirect(request.url)
 
-        # Save the uploaded ZIP file
-        file.save(zip_filename)
+    with TemporaryDirectory() as temp_dir1, TemporaryDirectory() as temp_dir2:
+        zip_filename1 = secure_filename(zip_file1.filename)
+        zip_filename2 = secure_filename(zip_file2.filename)
 
-        # Create a folder to extract the contents
-        extract_folder = 'extracted_files'
-        os.makedirs(extract_folder, exist_ok=True)
+        zip_file_path1 = os.path.join(temp_dir1, zip_filename1)
+        zip_file_path2 = os.path.join(temp_dir2, zip_filename2)
 
-        # Extract the contents of the ZIP file
-        with zipfile.ZipFile(zip_filename, 'r') as zip_ref:
-            zip_ref.extractall(extract_folder)
+        zip_file1.save(zip_file_path1)
+        zip_file2.save(zip_file_path2)
+
+        # Create folders to extract the contents
+        extract_folder1 = os.path.join('extracted_files', zip_filename1.split('.')[0])
+        os.makedirs(extract_folder1, exist_ok=True)
+
+        extract_folder2 = os.path.join('extracted_files', zip_filename2.split('.')[0])
+        os.makedirs(extract_folder2, exist_ok=True)
+
+        # Extract the contents of the ZIP files
+        with zipfile.ZipFile(zip_file_path1, 'r') as zip_ref1, zipfile.ZipFile(zip_file_path2, 'r') as zip_ref2:
+            zip_ref1.extractall(extract_folder1)
+            zip_ref2.extractall(extract_folder2)
+
+        
 
         return redirect('/results')
 
 @app.route('/results')
 def show_results():
-    import cv2
-    import numpy as np
-    import glob
-    from cell_count import generate_plot
-    
-    import matplotlib.pyplot as plt
    
-    uploaded_zip_filename = session.get('uploaded_zip_filename', None)
-    folder_name = os.path.splitext(uploaded_zip_filename)[0]
-    folder_path = os.path.join('extracted_files/', folder_name)
-    
-    generate_plot ()
-    
-    return render_template('results.html')
+
+        generate_plot('/Users/gmacheta/Documents/GitHub/cellCS/extracted_files/SHSY5Y_Rep_1/SHSY5Y Rep 1','cell_count_1')
+
+        generate_plot('/Users/gmacheta/Documents/GitHub/cellCS/extracted_files/SHSY5Y_Rep_2/SHSY5Y Rep 2','cell_count_2')
+        # Create a unique graph filename
+       
+        
+        return render_template('results.html')
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory('static', filename)
 
 if __name__ == '__main__':
-    app.secret_key = 'your_secret_key'  # Add a secret key for session
     app.run(debug=True)
